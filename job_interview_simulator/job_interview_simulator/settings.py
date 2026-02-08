8c39e02d1147f70da983f6452219d7328a2bc9d1
Its-Jyoti <162825823+Its-Jyoti@users.noreply.github.com>
2026-02-09 00:00:54 +0530

    Update settings.py

--- job_interview_simulator/job_interview_simulator/settings.py
+++ job_interview_simulator/job_interview_simulator/settings.py
@@ -86,8 +86,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 # settings.py
-//CELERY_BROKER_URL = 'redis://localhost:6379/0'
-//CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
