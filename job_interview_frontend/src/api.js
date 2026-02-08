e9a535298ae59c594333acc14d04fb48c2f3e477
Its-Jyoti <162825823+Its-Jyoti@users.noreply.github.com>
2026-02-08 14:40:33 +0530

    Update api.js

--- job_interview_frontend/src/api.js
+++ job_interview_frontend/src/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/`;
 
 export const createInterviewPreference = async (preferenceData) => {
     try {
